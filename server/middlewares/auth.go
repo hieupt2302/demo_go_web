356bff5d5135f810138ac68ed48c0a8b84529752
Phạm Trung Hiếu(G1) <hieupt@gmo.local>
2026-01-08 11:33:01 +0700

    authentication

---
+++ server/middlewares/auth.go
@@ -0,0 +1,40 @@
+package middlewares
+
+import (
+	"strings"
+
+	"demowebgo/utlis"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(401, gin.H{"error": "Missing Authorization header"})
+			c.Abort()
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(401, gin.H{"error": "Invalid Authorization format"})
+			c.Abort()
+			return
+		}
+
+		token, err := utils.ValidateToken(parts[1])
+		if err != nil || !token.Valid {
+			c.JSON(401, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		c.Set("user_id", uint(claims["user_id"].(float64)))
+
+		c.Next()
+	}
+}
---
+++ server/utlis/jwt.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"time"
+	"os"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+func GenerateToken(userID uint) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(), // 1 ngày
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+func ValidateToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+}
